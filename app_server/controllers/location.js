0cd7fc86d4f6370a277df4b6c0e8f81b49b139ec
George Kolesnikov <gkolesnikov88@gmail.com>
2020-11-01 09:27:59 +0300

    Created dynamic views using existing api

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,5 +1,74 @@
-/* GET 'home' page */
-const homelist = (req, res) => {
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = 'https://glacial-bayou-57015.herokuapp.com';
+}
+
+const showError = (req, res, status) => {
+    let title = '';
+    let content = '';
+    if (status === 404) {
+        title = '404, page not found';
+        content = 'We can\'t find this page. Sorry.';
+    } else {
+        title = `${status}, something's gone wrong`;
+        content = 'Oops, something has gone wrong.';
+    }
+    res.status(status);
+    res.render('generic-text', {
+        title,
+        content
+    });
+}
+
+const formatDistance = (distance) => {
+    let thisDistance = 0;
+    let unit = ' m';
+    if (distance > 1000) {
+        thisDistance = parseFloat(distance / 1000).toFixed(1);
+        unit = ' km';
+    } else {
+        thisDistance = Math.floor(distance);
+    }
+    return thisDistance + unit;
+};
+
+const getLocationInfo = (req, res, callback) => {
+    const path = `/api/locations/${req.params.locationid}`;
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {}
+    };
+    request(
+        requestOptions,
+        (err, {statusCode}, body) => {
+            const data = body;
+            if (statusCode === 200) {
+                data.coords = {
+                    lng: body.coords[1],
+                    lat: body.coords[0],
+                };
+                callback(req, res, data);
+            } else {
+                showError(req, res, statusCode);
+            }
+        }
+    );
+};
+
+const renderHomepage = (req, res, responseBody) => {
+    let message = null;
+    if (!(responseBody instanceof Array)) {
+        message = "API lookup error";
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = "No places found nearby";
+        }
+    }
     res.render('locations-list', {
         title: 'Locator - find a place to work with wifi',
         pageHeader: {
@@ -9,92 +78,124 @@ const homelist = (req, res) => {
         sidebar: `Looking for wifi and a seat? Loc8r helps you find places to work when out and about.
             Perhaps with coffee, cake or a pint?
             Let Loc8r help you find the place you're looking for.`,
-        locations: [{
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-        },{
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-        },{
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-        }]
+        locations: responseBody,
+        message
     });
 };
 
-/* GET 'Location info' page */
-const locationInfo = (req, res) => {
+const hasValidationError = (body) => {
+    let result = false;
+    if (body.errors) {
+        for (const errorKey in body.errors) {
+            const error = body.errors[errorKey];
+            if (error.name && error.name === 'ValidatorError') {
+                result = true;
+            }
+        }
+    }
+    return result;
+}
+
+/* GET 'home' page */
+const homelist = (req, res) => {
+    const path = '/api/locations';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+        qs: {
+            lng: -0.7992599,
+            lat: 51.378091,
+            maxDistance: 20
+        }
+    };
+    request(requestOptions, (err, {statusCode}, body) => {
+        let data = [];
+        if (statusCode === 200 && body.length) {
+            data = body.map(item => {
+                item.distance = formatDistance(item.distance);
+                return item;
+            });
+        } else if (statusCode === 404) {
+            data = null;
+        }
+        renderHomepage(req, res, data);
+    })
+};
+
+const renderDetailPage = (req, res, location) => {
     res.render('location-info', {
-            title: 'Starcups',
+            title: location.name,
             pageHeader: {
-                title: 'Locator',
+                title: location.name,
             },
             sidebar: {
                 context: 'is on Locator because it has accessible wifi and space to sit down with your laptop and get some work done.',
                 callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
             },
-            location: {
-                name: 'Starcups',
-                address: '125 High Street, Reading, RG6 1PS',
-                rating: 3,
-                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-                coords: {lat: 39.724972, lng: 43.601930},
-                openingTimes: [
-                    {
-                        days: 'Monday - Friday',
-                        opening: '7:00am',
-                        closing: '7:00pm',
-                        closed: false
-                    },
-                    {
-                        days: 'Saturday',
-                        opening: '8:00am',
-                        closing: '5:00pm',
-                        closed: false
-                    },
-                    {
-                        days: 'Sunday',
-                        closed: true
-                    }
-                ],
-                reviews: [
-                    {
-                        author: 'Simon Holmes',
-                        rating: 5,
-                        timestamp: '16 July 2013',
-                        reviewText: 'What a great place. I can\'t say enough good things about it.'
-                    },
-                    {
-                        author: 'Charlie Chaplin',
-                        rating: 3,
-                        timestamp: '16 June 2013',
-                        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-                    }
-                ]
-            }
+            location
         }
     );
+}
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+    getLocationInfo(req, res,
+        (req, res, data) => renderDetailPage(req, res, data));
+};
+
+const renderReviewForm = (req, res, {name}) => {
+    console.log(req.query);
+    res.render('location-review-form', {
+        title: `Review ${name} on Loc8r`,
+        pageHeader: { title: `Review ${name}` },
+        error: req.query.err
+    });
 };
 
 /* GET 'Add review' page */
 const addReview = (req, res) => {
-    res.render('location-review-form', {
-        title: 'Add review',
-        pageHeader: { title: 'Review Starcups' }
-    })
+    getLocationInfo(req, res,
+        (req, res, data) => renderReviewForm(req, res, data));
+}
+
+
+
+const doAddReview = (req, res) => {
+    const locationid = req.params.locationid;
+    const path = `/api/locations/${locationid}/reviews`;
+    const postdata = {
+        author: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        reviewText: req.body.review
+    };
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'POST',
+        json: postdata
+    };
+    if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+        res.redirect(`/location/${locationid}/review/new?err=val`);
+    } else {
+        request(
+            requestOptions,
+            (err, {statusCode}, body) => {
+                const name = body.name;
+                if (statusCode === 201) {
+                    res.redirect(`/location/${locationid}`);
+                } else if (statusCode === 400 && hasValidationError(body)) {
+                    res.redirect(`/location/${locationid}/review/new?err=val`);
+                } else {
+                    showError(req, res, statusCode);
+                }
+            }
+        );
+    }
 }
 
 module.exports = {
     homelist,
     locationInfo,
     addReview,
+    doAddReview,
 }
